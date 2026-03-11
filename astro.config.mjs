d4c11a080d846c71be0a43884635797043b6f7e5
yu-kasai0929 <yu.kasai@geekly.co.jp>
2026-03-11 10:15:58 +0900

    git管理対象変更とvertex ai導入に向けたセットアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+
   vite: {
     plugins: [tailwindcss()],
   },
