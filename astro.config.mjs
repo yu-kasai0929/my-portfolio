0bce4959cf1efc82914e04a344ff343286ba8e6b
yu-kasai0929 <yu.kasai@geekly.co.jp>
2026-03-11 15:05:01 +0900

    Vercel専用の画像最適化機能をONにする

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-
+  imageService: true,
   vite: {
     plugins: [tailwindcss()],
   },
